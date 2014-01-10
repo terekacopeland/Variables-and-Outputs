c929299b18a0bad50f1a6ec402eb0aa552dc9f60
terekacopeland <starsxmexico@live.com>
2014-01-10 01:25:33 -0500

    lab 2

---
+++ copeland_tereka_lab2.js
@@ -0,0 +1,36 @@
+//alert("JavaScript works!");
+
+//Tereka Copeland
+//SDI 1401
+//Lab 2
+//Varibles and Outputs
+
+//My Variables
+var myRoommateName = "";
+var hasFlat = true;
+var numOfFlatTires = 1;
+var changeTire = true;
+var changeTireF = false;
+var myRoommateName = prompt("What is your roommate's name?");
+
+
+//My Outputs
+console.log("Leaving work at 1:30am with my coworker and I realize I have a flat tire.");
+console.log("I check how many flat tires are on the car.");
+console.log("I have " + numOfFlatTires + " flat tire.");
+console.log("I decide to call my roommate to help me change the tire.");
+console.log("My coworker asks me 'what is my roommates name?'");
+console.log("My roommate's name is " + myRoommateName + ".");
+
+console.log("I need my roommate, " + myRoommateName + " to help me change my flat tire.");
+
+changeTire = confirm("Do you want to help me? OK for yes, Cancel for no.");
+
+console.log("I ask my coworker true or false if they think my roommate will help me change my tire,\nthey said " + changeTire + ".\n");
+
+//console.log("I ask my roommate if they will help me change my tire,\nit is " + changeTireF + " my roommate is not on their way to help me.\n");
+
+//changeTireF = confirm("Do you want to help me? OK for yes, Cancel for no.");
+
+MyRoommateName = prompt("What is your roommate's name again?" , ""); 
+
